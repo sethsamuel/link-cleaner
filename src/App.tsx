a5d8e6ed90484210b3afb6c48f3cf20fa04d47da
Seth Samuel <sethfsamuel@gmail.com>
2025-06-15 16:32:29 -0400

    Clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import css from "./App.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
